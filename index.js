0545124180874d56ca31f4c16c7502c9642e1dc6
solankidharmik4821 <solankidharmik4821@gmail.com>
2026-02-12 19:36:55 +0530

    commit 12/2/2026

--- index.js
+++ index.js
@@ -29,38 +29,10 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
-function send() {
- let fname = document.getElementById("fname").value;
- let lname = document.getElementById("lname").value;
- let mail = document.getElementById("mail").value;
- let msg = document.getElementById("msg").value;
-
-  emailjs.send("service_y9yajl3", "template_c32jilo",  {
-    from_name: "Admin",
-    to_name: fname +' '+lname,
-    message: msg,
-    from_email: mail,
-  },"lwvsxuBuPuY1jzTqm").then(
-    function (response) {
-      console.log('SUCCESS!', response.status, response.text);
-    },
-    function (err) {
-      console.log('FAILED...', err);
-    },
-  );
-
-  alert("Thank you for showing your interest.");
-}
-
 AOS.init();
 
 // 'pie' is class name div
 const circle = new CircularProgressBar("pie");
 circle.initial();
 
-(function(){
-  emailjs.init({
-    publicKey: "YOUR_PUBLIC_KEY",
-  });
-})();
 
